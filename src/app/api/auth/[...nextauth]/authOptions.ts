f939c43be91a8a52681d3d737576fee0efaf2797
nusrat.jahan <86945412+NusratTisha7@users.noreply.github.com>
2023-11-15 12:10:28 +0600

    Add your commit

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -47,10 +47,9 @@ export const getNextAuthOptions = () => {
       AppleProvider({
         clientId: <string>process.env.APPLE_CLIENT_ID,
         clientSecret: <any>{
-          // teamId: process.env.APPLE_TEAM_ID,
-          // privateKey: process.env.APPLE_PRIVATE_KEY,
-          // keyId: process.env.APPLE_KEY_ID,
-          clientSecret: process.env.APPLE_PRIVATE_KEY,
+          teamId: process.env.APPLE_TEAM_ID,
+          privateKey: process.env.APPLE_PRIVATE_KEY,
+          keyId: process.env.APPLE_KEY_ID,
         },
       }),
       CredentialsProvider({
