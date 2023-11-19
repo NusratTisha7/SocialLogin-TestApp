c56161c371d0ef45e89927376fa6ac616c673c9b
Tisha <tisha@thedoodleinc.com>
2023-11-20 01:22:06 +0600

    ..

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -17,7 +17,7 @@ export const getNextAuthOptions = () => {
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
       async signIn({ user, account, profile }: any) {
-        console.log("user",user);
+        console.log("user", user);
         return true;
       },
       async redirect({ url, baseUrl }) {
@@ -48,59 +48,59 @@ export const getNextAuthOptions = () => {
           clientSecret: <string>process.env.GOOGLE_CLIENT_SECRET,
       }),
             AppleProvider({
-              clientId: "app.vercel.social-login-test-app",
-                clientSecret: <any>{
-                  teamId: "YURBA43A7C",
-                privateKey: "eyJhbGciOiJFUzI1NiIsImtpZCI6IjJZVjc2VVZRVloifQ.eyJleHAiOjE3MDMwMDYyNzEsImlhdCI6MTcwMDQxNDI0MSwiaXNzIjoiWVVSQkE0M0E3QyIsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJhcHAudmVyY2VsLnNvY2lhbC1sb2dpbi10ZXN0LWFwcCJ9.A8HGpUkJL10zkVITIfwnE5cdx3AIRGIi3n87VGm7bdcuUuIeBMinQaURiopb_hpa5ADdNSFAITvphrlmJR34ag",
-                keyId: "2YV76UVQVZ",
+              clientId: "app.vercel.firsttrip.test",
+            clientSecret: <any>{
+              teamId: "YURBA43A7C",
+              privateKey: "eyJhbGciOiJFUzI1NiIsImtpZCI6IkoyNjlOVk1CNTMifQ.eyJleHAiOjE3MDMwMTM0NjIsImlhdCI6MTcwMDQyMTQzMiwiaXNzIjoiWVVSQkE0M0E3QyIsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJhcHAudmVyY2VsLmZpcnN0dHJpcC50ZXN0In0.GQz02s5iST6vcC8fl0z0XUF6hbs0gjIVbRA8yrCvFGZHC0ZmVSsPBOWq4MWgnhWhIotF8tzW5Bxlw_YY6lCJ0Q",
+              keyId: "J269NVMB53",
         },
       }),
-                CredentialsProvider({
-                  name: "Sign in",
-                credentials: {
-                  username: {
-                  label: "Username",
-                type: "username",
-                placeholder: "example@example.com",
+              CredentialsProvider({
+                name: "Sign in",
+              credentials: {
+                username: {
+                label: "Username",
+              type: "username",
+              placeholder: "example@example.com",
           },
-                password: {label: "Password", type: "password" },
+              password: {label: "Password", type: "password" },
         },
 
-                async authorize(credentials: any) {
+              async authorize(credentials: any) {
           return credentials;
         },
       }),
-                ],
+              ],
 
-                cookies: {
-                  callbackUrl: {
-                  name: 'https://social-login-test-app.vercel.app',
-                options: {
-                  httpOnly: false,
-                sameSite: 'none',
-                path: '/',
-                secure: true,
+              cookies: {
+                callbackUrl: {
+                name: 'https://firsttrip.vercel.app',
+              options: {
+                httpOnly: false,
+              sameSite: 'none',
+              path: '/',
+              secure: true,
                     },
                   },
-                pkceCodeVerifier: {
-                  name: "next-auth.pkce.code_verifier",
-                options: {
-                  httpOnly: true,
-                sameSite: "none",
-                path: "/",
-                secure: true,
+              pkceCodeVerifier: {
+                name: "next-auth.pkce.code_verifier",
+              options: {
+                httpOnly: true,
+              sameSite: "none",
+              path: "/",
+              secure: true,
         },
       },
     },
 
-                session: {
-                  maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
-    },
-                jwt: {
-                  secret: process.env.NEXTAUTH_SECRET,
+              session: {
                 maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
+    },
+              jwt: {
+                secret: process.env.NEXTAUTH_SECRET,
+              maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
     },
   };
 
-                return authOptions;
+              return authOptions;
 };
