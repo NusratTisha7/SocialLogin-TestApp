bef00a58af32b23d753217a4f38aff26b02f1eb4
Md. Nazmul Hasan Sourab <mdnhs.cse@gmail.com>
2023-11-20 04:15:39 +0600

    new package add

--- src/app/component/signin.tsx
+++ src/app/component/signin.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { signIn } from "next-auth/react";
+import { useState } from "react";
+import AppleSignin from "react-apple-signin-auth";
 
 const Signin = () => {
+  const [info, setInfo] = useState();
+  console.log(info,"%%")
   return (
     <div className="mt-3 grid grid-cols-2 gap-2">
       <div
@@ -12,12 +16,42 @@ const Signin = () => {
       </div>
       <div
         className="flex cursor-pointer items-center justify-center rounded-lg bg-[#F6F6F6]"
-        onClick={() => signIn("apple", { callbackUrl: 'https://firsttrip.vercel.app/api/auth/callback/apple' },
-          { redirect_uri: "https://firsttrip.vercel.app/api/auth/callback/apple" })}
+        onClick={() =>
+          signIn(
+            "apple",
+            {
+              callbackUrl:
+                "https://firsttrip.vercel.app/api/auth/callback/apple",
+            },
+            {
+              redirect_uri:
+                "https://firsttrip.vercel.app/api/auth/callback/apple",
+            }
+          )
+        }
       >
         <p className="ml-1 text-xs text-[#575757]">Sign in with Apple</p>
       </div>
-      <p className="text-white">Counter: 6</p>
+      <p className="text-white">Counter: 8 </p>
+
+      <AppleSignin
+        authOptions={{
+          clientId: "app.vercel.firsttrip.test",
+          scope: "email name",
+          redirectURI: "https://firsttrip.vercel.app/api/auth/callback/apple",
+          state: "state",
+          nonce: "nonce",
+          usePopup: true,
+        }}
+        uiType="dark"
+        className="apple-auth-btn"
+        noDefaultStyle={false}
+        buttonExtraChildren="Continue with Apple"
+        onSuccess={(response: any) => setInfo(response)}
+        onError={(error: any) => console.error(error)}
+        iconProp={{ style: { marginTop: "10px" } }}
+        render={(props: any) => <button className=" cursor-pointer bg-pink-500 px-5 py-1.5 rounded-lg" {...props}>Faltu Login</button>}
+      />
     </div>
   );
 };
