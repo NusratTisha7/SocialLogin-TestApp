7f6ad909b2407bfe1a5c2e18848b166538e0bfaa
Mofajjal Rasul <mofajjal.rasul@technonext.com>
2023-11-21 13:20:19 +0600

    fixup apple creds
    Signed-off-by: Mofajjal Rasul <mofajjal.rasul@technonext.com>

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,8 +1,7 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 import AppleProvider from "next-auth/providers/apple";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { cookies } from "next/headers";
 
 export const getNextAuthOptions = () => {
@@ -10,7 +9,7 @@ export const getNextAuthOptions = () => {
     try {
       const cookieStore = cookies();
       return cookieStore.get("remember")?.value === "true" ? true : false;
-    } catch (error: any) { }
+    } catch (error: any) {}
   };
 
   const authOptions: NextAuthOptions = {
@@ -21,7 +20,7 @@ export const getNextAuthOptions = () => {
         return true;
       },
       async redirect({ url, baseUrl }) {
-        return baseUrl
+        return baseUrl;
       },
       async jwt({ token, user, account, profile }) {
         if (typeof user !== typeof undefined) {
@@ -45,62 +44,67 @@ export const getNextAuthOptions = () => {
     providers: [
       GoogleProvider({
         clientId: <string>process.env.GOOGLE_CLIENT_ID,
-          clientSecret: <string>process.env.GOOGLE_CLIENT_SECRET,
+        clientSecret: <string>process.env.GOOGLE_CLIENT_SECRET,
       }),
-            AppleProvider({
-              clientId: "app.vercel.firsttrip.test",
-            clientSecret: <any>{
-              teamId: "YURBA43A7C",
-              privateKey: "eyJhbGciOiJFUzI1NiIsImtpZCI6IkoyNjlOVk1CNTMifQ.eyJleHAiOjE3MDMwMTM0NjIsImlhdCI6MTcwMDQyMTQzMiwiaXNzIjoiWVVSQkE0M0E3QyIsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJhcHAudmVyY2VsLmZpcnN0dHJpcC50ZXN0In0.GQz02s5iST6vcC8fl0z0XUF6hbs0gjIVbRA8yrCvFGZHC0ZmVSsPBOWq4MWgnhWhIotF8tzW5Bxlw_YY6lCJ0Q",
-              keyId: "J269NVMB53",
-        },
+      AppleProvider({
+        clientId: "app.vercel.firsttrip.test",
+        clientSecret:
+          "eyJhbGciOiJFUzI1NiIsImtpZCI6IkoyNjlOVk1CNTMifQ.eyJleHAiOjE3MDMwMTM0NjIsImlhdCI6MTcwMDQyMTQzMiwiaXNzIjoiWVVSQkE0M0E3QyIsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJhcHAudmVyY2VsLmZpcnN0dHJpcC50ZXN0In0.GQz02s5iST6vcC8fl0z0XUF6hbs0gjIVbRA8yrCvFGZHC0ZmVSsPBOWq4MWgnhWhIotF8tzW5Bxlw_YY6lCJ0Q",
       }),
-              CredentialsProvider({
-                name: "Sign in",
-              credentials: {
-                username: {
-                label: "Username",
-              type: "username",
-              placeholder: "example@example.com",
+
+      // clientId: "app.vercel.firsttrip.test",
+      // clientSecret: <any>{
+      //   teamId: "YURBA43A7C",
+      //   privateKey:
+      //     "eyJhbGciOiJFUzI1NiIsImtpZCI6IkoyNjlOVk1CNTMifQ.eyJleHAiOjE3MDMwMTM0NjIsImlhdCI6MTcwMDQyMTQzMiwiaXNzIjoiWVVSQkE0M0E3QyIsImF1ZCI6Imh0dHBzOi8vYXBwbGVpZC5hcHBsZS5jb20iLCJzdWIiOiJhcHAudmVyY2VsLmZpcnN0dHJpcC50ZXN0In0.GQz02s5iST6vcC8fl0z0XUF6hbs0gjIVbRA8yrCvFGZHC0ZmVSsPBOWq4MWgnhWhIotF8tzW5Bxlw_YY6lCJ0Q",
+      //   keyId: "J269NVMB53",
+      // },
+      CredentialsProvider({
+        name: "Sign in",
+        credentials: {
+          username: {
+            label: "Username",
+            type: "username",
+            placeholder: "example@example.com",
           },
-              password: {label: "Password", type: "password" },
+          password: { label: "Password", type: "password" },
         },
 
-              async authorize(credentials: any) {
+        async authorize(credentials: any) {
           return credentials;
         },
       }),
-              ],
+    ],
 
-              cookies: {
-                callbackUrl: {
-                name: 'https://firsttrip.vercel.app',
-              options: {
-                httpOnly: false,
-              sameSite: 'none',
-              path: '/',
-              secure: true,
-                    },
-                  },
-              pkceCodeVerifier: {
-                name: "next-auth.pkce.code_verifier",
-              options: {
-                httpOnly: true,
-              sameSite: "none",
-              path: "/",
-              secure: true,
+    cookies: {
+      callbackUrl: {
+        name: "https://firsttrip.vercel.app",
+        options: {
+          httpOnly: false,
+          sameSite: "none",
+          path: "/",
+          secure: true,
+        },
+      },
+      pkceCodeVerifier: {
+        name: "next-auth.pkce.code_verifier",
+        options: {
+          httpOnly: true,
+          sameSite: "none",
+          path: "/",
+          secure: true,
         },
       },
     },
 
-              session: {
-                maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
+    session: {
+      maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
     },
-              jwt: {
-                secret: process.env.NEXTAUTH_SECRET,
-              maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
+    jwt: {
+      secret: process.env.NEXTAUTH_SECRET,
+      maxAge: rememberMe() ? (30 * 24 * 60 * 60) / 4 : (24 * 60 * 60) / 4,
     },
   };
 
-              return authOptions;
+  return authOptions;
 };
