1f8098dddeaaac6238e076789690e2fd44b551c7
Tisha <86945412+NusratTisha7@users.noreply.github.com>
2024-09-02 17:47:50 +0600

    .

--- src/app/component/signin.tsx
+++ src/app/component/signin.tsx
@@ -91,7 +91,7 @@ const SignIn = () => {
     <div className="mt-3 grid grid-cols-2 gap-2">
       <div
         className="flex cursor-pointer items-center justify-center rounded-lg bg-[#F6F6F6] p-3"
-        onClick={() => signIn("google")}
+        onClick={() => handleGoogleSignIn}
       >
         <p className="ml-1 text-xs text-[#575757]">Sign in with Google</p>
       </div>
