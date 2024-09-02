c8349b71344a79d27fe8f32ce2690f3c35001286
Tisha <86945412+NusratTisha7@users.noreply.github.com>
2024-09-02 16:20:45 +0600

    .

--- src/app/component/signin.tsx
+++ src/app/component/signin.tsx
@@ -60,7 +60,7 @@ const SignIn = () => {
     <div className="mt-3 grid grid-cols-2 gap-2">
       <div
         className="flex cursor-pointer items-center justify-center rounded-lg bg-[#F6F6F6] p-3"
-        onClick={handleGoogleSignIn}
+        onClick={()=>handleGoogleSignIn}
       >
         <p className="ml-1 text-xs text-[#575757]">Sign in with Google</p>
       </div>
@@ -90,7 +90,7 @@ const SignIn = () => {
       >
         <p className="ml-1 text-xs text-[#575757]">Sign in with Apple</p>
       </div>
-      <p className="text-white">Counter: 17 </p>
+      <p className="text-white">Counter: 18 </p>
 
       <AppleSignin
         authOptions={{
