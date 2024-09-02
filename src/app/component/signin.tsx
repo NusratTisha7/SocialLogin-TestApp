719fcafff842473dfeba194a2b14ecb1e509dcf8
Tisha <86945412+NusratTisha7@users.noreply.github.com>
2024-09-02 11:31:19 +0600

    .

--- src/app/component/signin.tsx
+++ src/app/component/signin.tsx
@@ -38,6 +38,7 @@ const SignIn = () => {
   };
 
   const signInWithGooglePopup = () => {
+    console.log("..")
     const width = 500;
     const height = 600;
     const left = window.screen.width / 2 - width / 2;
@@ -66,7 +67,7 @@ const SignIn = () => {
     <div className="mt-3 grid grid-cols-2 gap-2">
       <div
         className="flex cursor-pointer items-center justify-center rounded-lg bg-[#F6F6F6] p-3"
-        onClick={() => signInWithGooglePopup}
+        onClick={signInWithGooglePopup}
       >
         <p className="ml-1 text-xs text-[#575757]">Sign in with Google</p>
       </div>
